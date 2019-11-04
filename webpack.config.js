685a32f0ea571c3671d03f2e5474a9221d447608
Precup <uidu3501@contiwan.com>
2019-11-04 16:15:51 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,58 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry:'./src/app.js',
-    output:{
-        path:path.join(__dirname, 'public'),
-        filename:'bundle.js'
-    },
+module.exports = (env) => {
+    const isProduction = env === 'production';
 
-    module:{
-        rules:[{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude: /node_modules/
-        }, {
-            test:/\.s?css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            }
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
 
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            }),
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+
+    };
 };
